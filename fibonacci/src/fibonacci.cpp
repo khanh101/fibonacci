5ea8210e06e99e74abb4ebaeb1d5ed1727671ab5
khanh <khanh.nguyen.contact@gmail.com>
2025-03-12 00:24:55 +0800

    auto_push

--- fibonacci/src/fibonacci.cpp
+++ fibonacci/src/fibonacci.cpp
@@ -3,45 +3,48 @@
 #include <cstdint>
 #include <gmpxx.h>
 
-// Q[√x] - a + b √x
-template<int x>
-struct QRX {
-  mpq_class a;
-  mpq_class b;
-  QRX(mpq_class a, mpq_class b = 1) : a(a), b(b) {}
-  ~QRX() {}
-  QRX<x> operator+(const QRX<x> &other) const {
-    const mpq_class& a1 = a;
-    const mpq_class& b1 = b;
-    const mpq_class& a2 = other.a;
-    const mpq_class& b2 = other.b;
-    return QRX<x>(a1 + a2, b1 + b2);
+using Z = mpz_class;
+using Q = mpq_class;
+
+// quadratic field Q[√x] - a + b √x
+template<int d>
+struct QF {
+  Q a;
+  Q b;
+  QF(Q a, Q b = 1) : a(a), b(b) {}
+  ~QF() {}
+  QF<d> operator+(const QF<d> &other) const {
+    const Q& a1 = a;
+    const Q& b1 = b;
+    const Q& a2 = other.a;
+    const Q& b2 = other.b;
+    return QF<d>(a1 + a2, b1 + b2);
   }
-  QRX<x> operator-(const QRX<x> &other) const {
-    const mpq_class& a1 = a;
-    const mpq_class& b1 = b;
-    const mpq_class& a2 = other.a;
-    const mpq_class& b2 = other.b;
-    return QRX<x>(a1 - a2, b1 - b2);
+  QF<d> operator-(const QF<d> &other) const {
+    const Q& a1 = a;
+    const Q& b1 = b;
+    const Q& a2 = other.a;
+    const Q& b2 = other.b;
+    return QF<d>(a1 - a2, b1 - b2);
   }
-  QRX<x> operator*(const QRX<x> &other) const {
-    const mpq_class& a1 = a;
-    const mpq_class& b1 = b;
-    const mpq_class& a2 = other.a;
-    const mpq_class& b2 = other.b;
-    return QRX<x>(a1 * a2 + b1 * b2 * x, a1 * b2 + b1 * a2);
+  QF<d> operator*(const QF<d> &other) const {
+    const Q& a1 = a;
+    const Q& b1 = b;
+    const Q& a2 = other.a;
+    const Q& b2 = other.b;
+    return QF<d>(a1 * a2 + b1 * b2 * d, a1 * b2 + b1 * a2);
   }
-  QRX<x> operator/(const QRX<x> &other) const {
-    const mpq_class& a1 = a;
-    const mpq_class& b1 = b;
-    const mpq_class& a2 = other.a;
-    const mpq_class& b2 = other.b;
-    mpq_class den = a2 * a2 - b2 * b2 * x;
-    return QRX<x>((a1 * a2 - b1 * b2 * x)/den, (b1 * a2 - a1 * b2 * x)/den);
+  QF<d> operator/(const QF<d> &other) const {
+    const Q& a1 = a;
+    const Q& b1 = b;
+    const Q& a2 = other.a;
+    const Q& b2 = other.b;
+    Q den = a2 * a2 - b2 * b2 * d;
+    return QF<d>((a1 * a2 - b1 * b2 * d)/den, (b1 * a2 - a1 * b2 * d)/den);
   }
-  QRX<x> pow(mpz_class n) const {
+  QF<d> pow(Z n) const {
     if (n == 0) {
-      return QRX<x>(1, 0);
+      return QF<d>(1, 0);
     }
     if (n % 2 == 0) {
       auto half = pow(n / 2);
@@ -53,12 +56,12 @@ struct QRX {
 };
 
 char* fibonacci(char* n_str) {
-  QRX<5> root5 = QRX<5>(0, 1); // 0 + √5
-  QRX<5> phi = QRX<5>(mpq_class(1, 2), mpq_class(1, 2)); // 1/2 + 1/2 √5
-  QRX<5> psi = QRX<5>(mpq_class(1, 2), mpq_class(-1, 2)); // 1/2 - 1/2 √5
+  QF<5> root5 = QF<5>(0, 1); // 0 + √5
+  QF<5> phi = QF<5>(Q(1, 2), Q(1, 2)); // 1/2 + 1/2 √5
+  QF<5> psi = QF<5>(Q(1, 2), Q(-1, 2)); // 1/2 - 1/2 √5
 
-  mpz_class n = mpz_class(n_str, 16); // convert base 16 to mpz
-  QRX<5> m = (phi.pow(n) - psi.pow(n)) / root5;
+  Z n = Z(n_str, 16); // convert base 16 to mpz
+  QF<5> m = (phi.pow(n) - psi.pow(n)) / root5;
   std::string m_str = m.a.get_num().get_str(16);
   uint64_t len = std::strlen(m_str.c_str());
   char* m_str_out = (char*) std::malloc((1 + len) * sizeof(char));
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import time
 from fibonacci import fibonacci
 import sys
 
-sys.set_int_max_str_digits(0) # no conversion limit
 
 def fibonacci_py(n: int) -> int:
     if n <= 1:
@@ -11,14 +10,15 @@ def fibonacci_py(n: int) -> int:
     for _ in range(n-1):
         l, r = r, l + r
     return r
+if __name__ == "__main__":
+    sys.set_int_max_str_digits(0) # no conversion limit
+    n = int(sys.argv[1])
 
-n = int(sys.argv[1])
-
-t1 = time.perf_counter()
-x = fibonacci(n)
-t2 = time.perf_counter()
-# y = fibonacci_py(n)
-t3 = time.perf_counter()
-# assert x == y, f"wrong result"
-print(f"c time: {t2-t1}")
-# print(f"py time: {t3-t2}")
+    t1 = time.perf_counter()
+    x = fibonacci(n)
+    t2 = time.perf_counter()
+    # y = fibonacci_py(n)
+    t3 = time.perf_counter()
+    # assert x == y, f"wrong result"
+    print(f"c time: {t2-t1}")
+    # print(f"py time: {t3-t2}")
